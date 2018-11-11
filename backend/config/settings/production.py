31ab55c5dc287191baa1a650cec95097d3ae6f90
Vlad <vlad.sivh@gmail.com>
2018-11-11 23:08:53 +0300

    116

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -91,3 +91,23 @@ CACHES = {
         "KEY_PREFIX": "example"
     }
 }
+
+# DATABASE CONFIGURATION START
+# ------------------------------------------------------------------------------
+POSTGRES_DATABASE = env('DJANGO_POSTGRES_DATABASE')
+POSTGRES_USER = env('DJANGO_POSTGRES_USER')
+POSTGRES_PASSWORD = env('DJANGO_POSTGRES_PASSWORD')
+POSTGRES_HOST = env('DJANGO_POSTGRES_HOST')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': POSTGRES_DATABASE,
+        'USER': POSTGRES_USER,
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': POSTGRES_HOST,
+        'PORT': '5432',
+    }
+}
+# DATABASE CONFIGURATION END
+# ------------------------------------------------------------------------------
